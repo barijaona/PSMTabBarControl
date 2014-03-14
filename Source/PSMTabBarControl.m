a046788a9a123c6d0779910b1370510cea630875
Michael Ehrmann <michael@codingmonkeys.de>
2014-03-14 17:15:38 +0100

    [FIX] make sure overflow button position is calculated from the current cell position. stabilizes position while live resizing window.

--- Source/PSMTabBarControl.m
+++ Source/PSMTabBarControl.m
@@ -1252,7 +1252,6 @@ static NSMutableDictionary *registeredStyleClasses;
 	NSMenu *overflowMenu = [_controller overflowMenu];
 	[_overflowPopUpButton setHidden:(overflowMenu == nil)];
 	[_overflowPopUpButton setMenu:overflowMenu];
-    [self _positionOverflowMenu];        
 
 	if(_animationTimer) {
 		[_animationTimer invalidate];
@@ -1290,6 +1289,7 @@ static NSMutableDictionary *registeredStyleClasses;
         [self updateTrackingAreas];
 		[self setNeedsDisplay:YES];
 	}
+	[self _positionOverflowMenu];
 }
 
 - (void)_animateCells:(NSTimer *)timer {
@@ -1370,6 +1370,7 @@ static NSMutableDictionary *registeredStyleClasses;
         [self updateTrackingAreas];
 	}
 
+	[self _positionOverflowMenu];
 	[self setNeedsDisplay:YES];
 }
 
